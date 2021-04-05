5218ad90e093508c2570fad2cf9a0713f2c67990
Sebastien Lemetter <sebaleme@gmail.com>
2021-04-05 23:44:01 +0200

    Tutorial 2: start another activity, prepare first app (upload images)

--- app/src/main/java/com/example/helloworldlsm1so/MainActivity.kt
+++ app/src/main/java/com/example/helloworldlsm1so/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.helloworldlsm1so
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.example.helloworldlsm1so.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.editTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+
+    /** Called when the user taps the Guldo Button
+    fun sendMessageGuldo(view: View) {
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+    }
+
+
+    /** Called when the user taps the Vegeta Button */
+    fun sendMessageVegeta(view: View) {
+        // Do something in response to button
+    }*/
 }
